02414764b71162037ba4119567be37a348df6ee4
toheed-revolvinggames <toheed@revolvinggames.com>
2025-07-10 15:28:22 +0500

    implemented debounce in Search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 // means we can use event listeners and hooks
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -10,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathName = usePathname();
   const {replace} = useRouter();
 
-  function handleSearch(term: string){
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching ... ${term}`);
     const params = new URLSearchParams(searchParams);
     if(term){
@@ -21,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     replace(`${pathName}?${params.toString()}`);
     
-  }
+  }, 300);
   
   return (
     <div className="relative flex flex-1 flex-shrink-0">
